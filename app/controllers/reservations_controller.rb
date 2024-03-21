a16566ee2685cf6032e0fc7a2ac44cfa0c0dfa50
mikayakouta <mikayakouta@penguin>
2024-03-21 01:48:59 +0100

    reservation success created with BookListing model

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -20,56 +20,70 @@ class ReservationsController < ApplicationController
   end
   
     def create
-      @listing = Listing.find(params[:listing_id])
-      @reservation= @listing.reservations.new(reservation_params)
-      @reservation.listing = @listing
-      @reservation.guest = current_user
-  
-      if @reservation.save 
-        # create checkout session
-        listing = @reservation.listing
-        checkout_session = Stripe::Checkout::Session.create(
-          success_url: listing_reservation_url(@listing, @reservation),
-          cancel_url: listing_url(listing),
-          customer: current_user.stripe_customer_id,
-          mode: 'payment',
-          line_items: [{
-            price_data: {
-              unit_amount: listing.nightly_price,
-              currency: 'eur', # Corrected currency code to 'eur'
-              product: listing.stripe_product_id,
-            }, 
-            quantity: 1 # How many nights they booked?
-          }, {
-            price_data: {
-              unit_amount: listing.cleaning_fee,
-              currency: 'eur', # Corrected currency code to 'eur'
-              product: 'prod_PkaFnbaqbZmyqs', # Cleaning_fee product_id
-            },
-            quantity: 1
-          }],
-          metadata: {
-            reservation_id: @reservation.id,
-          },
-          payment_intent_data: {
-            #application_fee_amount: ((listing.cleaning_fee + listing.nightly_price) * 0.10).to_i,
-            # transfer_data: {
-            # destination: listing.host.stripe_account_id
+      @booking = BookListing.new(current_user, reservation_params)
 
-          # },
-            metadata: {
-              reservation_id: @reservation.id,
-
-            }
-
-          }
-        )
-        @reservation.update(session_id: checkout_session.id)
-        redirect_to checkout_session.url, allow_other_host: true
+      if @booking.save
+        render json: {
+          message: 'Booking created successfully',
+        }
       else
-        render :'listing/show', status: :unprocessable_entity
+        flash.now[:errors] = @booking.errors
+        render :new
       end
-    end
+
+
+
+
+      # @listing = Listing.find(params[:listing_id])
+      # @reservation= @listing.reservations.new(reservation_params)
+      # @reservation.listing = @listing
+      # @reservation.guest = current_user
+  
+      # if @reservation.save 
+      #   # create checkout session
+      #   listing = @reservation.listing
+      #   checkout_session = Stripe::Checkout::Session.create(
+      #     success_url: listing_reservation_url(@listing, @reservation),
+      #     cancel_url: listing_url(listing),
+      #     customer: current_user.stripe_customer_id,
+      #     mode: 'payment',
+      #     line_items: [{
+      #       price_data: {
+      #         unit_amount: listing.nightly_price,
+      #         currency: 'eur', # Corrected currency code to 'eur'
+      #         product: listing.stripe_product_id,
+      #       }, 
+      #       quantity: 1 # How many nights they booked?
+      #     }, {
+      #       price_data: {
+      #         unit_amount: listing.cleaning_fee,
+      #         currency: 'eur', # Corrected currency code to 'eur'
+      #         product: 'prod_PkaFnbaqbZmyqs', # Cleaning_fee product_id
+      #       },
+      #       quantity: 1
+      #     }],
+      #     metadata: {
+      #       reservation_id: @reservation.id,
+      #     },
+      #     payment_intent_data: {
+      #       #application_fee_amount: ((listing.cleaning_fee + listing.nightly_price) * 0.10).to_i,
+      #       # transfer_data: {
+      #       # destination: listing.host.stripe_account_id
+
+      #     # },
+      #       metadata: {
+      #         reservation_id: @reservation.id,
+
+      #       }
+
+      #     }
+      #   )
+      #   @reservation.update(session_id: checkout_session.id)
+      #   redirect_to checkout_session.url, allow_other_host: true
+      # else
+    #     render :'listing/show', status: :unprocessable_entity
+    #   end
+     end
   
 
     def cancel
@@ -89,7 +103,7 @@ class ReservationsController < ApplicationController
     end
   
     def reservation_params
-      params.require(:reservation).permit(:listing_id, :guest_id, :session_id, :status)
+      params.require(:reservation).permit(:listing_id, :guest_id, :session_id, :status, :start_date, :end_date)
     end
   
 
